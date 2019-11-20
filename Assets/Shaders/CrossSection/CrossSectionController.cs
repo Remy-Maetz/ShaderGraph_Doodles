02bd31830b70ada1451bb3e91548d96b83c73f01
Remy Maetz <remym@unity3d.com>
2019-11-20 16:04:06 +0100

    Add cross section shader

---
+++ Assets/Shaders/CrossSection/CrossSectionController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[RequireComponent(typeof(Renderer))]
+[ExecuteAlways]
+public class CrossSectionController : MonoBehaviour
+{
+    public string planePropertyName = "_Plane_";
+    public Transform planeTransform;
+
+    Renderer rndr;
+
+    void Start()
+    {
+        rndr = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (planeTransform == null) return;
+        
+        rndr.material.SetVector($"{planePropertyName}Position", planeTransform.position);
+        rndr.material.SetVector($"{planePropertyName}Normal", planeTransform.up);
+        rndr.material.SetVector($"{planePropertyName}Tangent", planeTransform.right);
+    }
+}
